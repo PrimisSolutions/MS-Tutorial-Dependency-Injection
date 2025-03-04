b06b419f8e0aeae938237dcd1435f2595fe4a344
DavidEvesPrimis <139118097+PrimisSolutions@users.noreply.github.com>
2025-03-04 16:36:32 +0000

    Final Commit

---
+++ Interfaces/IWelcomeService.cs
@@ -0,0 +1,7 @@
+namespace DependencyInjection.Interfaces
+{
+    public interface IWelcomeService
+    {
+        string GetWelcomeMessage();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using DependencyInjection.Interfaces;
+using DependencyInjection.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+/*
+There are three 'scopes' for adding a dependency:
+- Singleton: Service is available for the lifetime of the app, useful for e.g. reading configuration files at runtime
+- Scoped: Service is available for the lifetime of the request, useful for e.g. database connections
+- Transient: Service is created each time it is requested, useful for e.g. services that are stateless
+*/
+builder.Services.AddScoped<IWelcomeService, WelcomeService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (IWelcomeService service1, IWelcomeService service2) =>
+{
+    var message1 = service1.GetWelcomeMessage();
+    var message2 = service2.GetWelcomeMessage();
+    return Results.Text($"Service 1: {message1}\nService 2: {message2}");
+});
 
 app.Run();
